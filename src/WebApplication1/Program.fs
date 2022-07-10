ddcfea098516d650f7712de1627e193c08ef445d
SeungYongShim <syshim@me.com>
2022-07-10 14:29:58 +0900

    .

--- src/WebApplication1/Program.fs
+++ src/WebApplication1/Program.fs
@@ -1,6 +1,33 @@
 open Microsoft.AspNetCore.Builder
 open Giraffe
 
+
+let sendEmailNotification emailSettings customer message =
+    ()
+
+let sendEmailFromHotmailAccount =
+    // Here I only apply the `emailSettings` parameter:
+    sendEmailNotification hotmailSettings
+
+
+let sendSmsNotification smsService apiKey customer message =
+    ()
+
+let sendSmsWithTwillio =
+    // Here I only apply the `smsService` and `apiKey` parameters:
+    sendSmsNotification twilioService twilioApiKey
+
+
+let completeOrder notify customer shoppingBasket =
+    notify customer "Your order has been received."
+    ()
+
+let completeOrderAndNotify =
+    emailSettings
+    |> sendEmailNotification
+    |> completeOrder
+
+
 let webApp = choose [
     route "/" >=> choose [
         POST >=> json "Hello world"
@@ -15,4 +42,5 @@ let app = builder.Build()
 app.UseGiraffe webApp
 
 app.Run()
+
 
